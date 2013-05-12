cb86b80f49e465056110f30a2433506c8d575735
mo0rhuhn <max-schrimpf@hotmail.com>
2013-05-12 21:05:09 +0200

    Ausgabe auf Console Part 2

--- src/main/java/ch/zhaw/hs/thesisvalidator/view/ConsoleObserver.java
+++ src/main/java/ch/zhaw/hs/thesisvalidator/view/ConsoleObserver.java
@@ -12,12 +12,14 @@ import java.io.PrintStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Set;
+
+import ch.zhaw.hs.thesisvalidator.model.ThesisValidator;
 
 /**
  * @author Max
@@ -25,14 +27,19 @@ import java.util.concurrent.Executors;
  */
 public class ConsoleObserver implements Observer {
 
-	private final DateFormat fmt = new SimpleDateFormat( "hh:mm:ss:SS" );
+	private final DateFormat logTsdFormat = new SimpleDateFormat("hh:mm:ss:SS");
 	private final File outFile;
 	private final ConsoleOutput outConsole;
+	private int curResidue;
+	private Date startTSD;
 
-	public ConsoleObserver(File outDirectory) {
+	public ConsoleObserver(File outDirectory, int startResidue) {
+		startTSD = new Date();
+
 		outFile = new File(outDirectory, "log.txt");
 		outConsole = new ConsoleOutput();
-		
+		curResidue = startResidue;
+
 		printStreams("HSThesisValidator - Software Projekt 2 - ZHAW FS 2013 - Reto Hablützel & Max Schrimpf");
 		redirectSystemStreams();
 	}
@@ -45,45 +52,72 @@ public class ConsoleObserver implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		Map<String, List<String>> results = (Map<String, List<String>>) arg;
-		
+
 		printStreams("----------------------------------------------------------------");
+		printStreams("Restklasse: " + curResidue);
+		printStreams("Untersuchte Permutationen: "
+				+ ThesisValidator.calculatePermutations(curResidue));
+		curResidue++;
+
+		Date stopTSD = new Date();
+		long difference = stopTSD.getTime() - startTSD.getTime();
+
+		long diffSeconds = difference / 1000 % 60;
+		long diffMinutes = difference / (60 * 1000) % 60;
+		long diffHours = difference / (60 * 60 * 1000) % 24;
+
+		// sofort wieder Messung starten
+		startTSD = new Date();
+		printStreams("Benötigte Zeit ~ " + diffHours + ":" + diffMinutes + "." + diffHours);
+
 		if (results.size() == 0) {
 			printStreams("Satz war in allen Fällen gültig");
 		} else {
 			// sollte NIE vorkommen
-			// im ssh sind die values zu einem Key durch space getrennt
+			printStreams("Satz war nicht in allen Fällen gültig");
+			if (results.size() > 1) {
+				printStreams("irgendwas ist hier komisch");
+			}
+			Set<String> resultKey = results.keySet();
+			// Sollte nur einer sein
+			for (String key : resultKey) {
+				for (String aPermutation : results.get(resultKey)) {
+
+				}
+
+			}
 		}
-		
+
 	}
-	
-    /**
-     * Leitet die Standard Nachrichten Streams auf dieses Fenster um.
-     */
-    private void redirectSystemStreams() {
-        OutputStream out = new OutputStream() {
-            public void write(int b) throws IOException {
-            	printStreams(String.valueOf((char) b));
-            }
 
-            public void write(byte[] b, int off, int len) throws IOException {
-            	printStreams(new String(b, off, len));
-            }
+	/**
+	 * Leitet die Standard Nachrichten Streams auf dieses Fenster um.
+	 */
+	private void redirectSystemStreams() {
+		OutputStream out = new OutputStream() {
+			public void write(int b) throws IOException {
+				printStreams(String.valueOf((char) b));
+			}
 
-            public void write(byte[] b) throws IOException {
-                write(b, 0, b.length);
-            }
-        };
+			public void write(byte[] b, int off, int len) throws IOException {
+				printStreams(new String(b, off, len));
+			}
 
-        System.setOut(new PrintStream(out, true));
-        System.setErr(new PrintStream(out, true));
-    }
+			public void write(byte[] b) throws IOException {
+				write(b, 0, b.length);
+			}
+		};
 
-    public void printStreams(String line) {
-    	String tsd = fmt.format(Calendar.getInstance().getTime());
-    	
-    	outConsole.println(tsd + " " + line);
-    	fileWriteLn(tsd + " " + line);
-    }
+		System.setOut(new PrintStream(out, true));
+		System.setErr(new PrintStream(out, true));
+	}
+
+	public void printStreams(String line) {
+		String tsd = logTsdFormat.format(Calendar.getInstance().getTime());
+
+		outConsole.println(tsd + " " + line);
+		fileWriteLn(tsd + " " + line);
+	}
 
 	public void fileWriteLn(String line) {
 		BufferedWriter curFW = null;
